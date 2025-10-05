a0ca5738809978f4b19d7b7fc7cd42f9f0addc90
ShinigamiRyuk47 <adityabharambe45@gmail.com>
2025-10-05 00:23:14 -0400

    fix: update display settings for various sections and correct contact information

--- src/portfolio.js
+++ src/portfolio.js
@@ -112,7 +112,7 @@ const educationInfo = {
       schoolName: "Vishwakarma Institute of Technology",
       logo: require("./assets/images/vitLogo.png"),
       subHeader: "Bachelor of Technology in Mechanical Engineering",
-      duration: "August 2019 – May 2023",
+      duration: "August 2019 - May 2023",
       desc: "Completed undergraduate studies focused on robotics, automation, and mechanical design.",
       descBullets: [
         "Coursework: Robot Mechanics and Control, Machine Design, FEA, FEM, Data Structures, Data Science and ML, Hybrid Vehicles",
@@ -214,7 +214,7 @@ const bigProjects = {
       ]
     }
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Achievement Section
@@ -275,7 +275,7 @@ const achievementSection = {
       ]
     }
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Blogs Section
@@ -318,7 +318,7 @@ const talkSection = {
       event_url: "https://www.facebook.com/events/2339906106275053/"
     }
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Podcast Section
@@ -331,7 +331,7 @@ const podcastSection = {
   podcast: [
     "https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo"
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Resume Section
@@ -347,18 +347,18 @@ const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+92-0000000000",
-  email_address: "saadpasta70@gmail.com"
+  number: "+1-551-388-6227",
+  email_address: "adityabharambe45@gmail.com"
 };
 
 // Twitter Section
 
 const twitterDetails = {
   userName: "twitter", //Replace "twitter" with your twitter username without @
-  display: true // Set true to display this section, defaults to false
+  display: false // Set true to display this section, defaults to false
 };
 
-const isHireable = false; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
+const isHireable = true; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
 
 export {
   illustration,
